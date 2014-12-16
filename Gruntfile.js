08a613472607738b6c51260fd98b8c398ab36c28
Kai Birkenstock <kai.birkenstock@dcs-fuerth.de>
2014-12-16 12:28:51 +0100

    grunt tasks added: concurrent, cssmin, jshint, watch added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,42 @@
 module.exports = function (grunt) {
 
   require('time-grunt')(grunt);
-  require('jit-grunt')(grunt, {
-    htmlbuild: 'grunt-html-build'
-  });
+  require('node-sass');
+
+  require('jit-grunt')(grunt);
+
+  // the watch task (because of rename) can't be autoloaded
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-html-build');
 
   var target = grunt.option('target') || 'development';
 
   var userConfig = require('./build.config.js');
 
   var taskConfig = {
-    pkg: grunt.file.readJSON('package.json'),
-
     clean: {
       development: ['<%= devDir %>/*', '!<%= devDir %>/.gitkeep'],
       production: ['<%= prodDir %>/*', '!<%= prodDir %>/.gitkeep']
+    },
+
+    concurrent: {
+      build: {
+        //tasks: ['bower-install', 'html2js', 'traceur:appjs', 'less:build'],
+        tasks: ['traceur', 'sass'],
+        options: {
+          logConcurrentOutput: false
+        }
+      },
+      rebuildjs: {
+        tasks: [
+          //'newer:jshint:src',
+          //'newer:traceur:appjs'
+        ]
+      },
+      firstrun: {
+        tasks: ['index:development']
+        //tasks: ['index:development', 'karmaconfig']
+      }
     },
 
     connect: {
@@ -34,7 +56,11 @@ module.exports = function (grunt) {
 
     copy: {
       vendorJs: {
-        src: ['<%= vendorFiles.js %>', 'src/main.js', 'vendor/requirejs/require.js'],
+        src: ['<%= vendorFiles.js %>'],
+        dest: '<%= devDir %>/'
+      },
+      vendorCss: {
+        src: ['<%= vendorFiles.css %>'],
         dest: '<%= devDir %>/'
       },
       assets: {
@@ -45,26 +71,114 @@ module.exports = function (grunt) {
       }
     },
 
-    htmlbuild: {
+    cssmin: {
+      combine: {
+        files: {
+          '<%= devDir %>/assets/<%= pkg.name %>-<%= pkg.version %>.css':
+            ['<%= vendorFiles.css %>', '<%= devDir %>/assets/<%= pkg.name %>-<%= pkg.version %>.css']
+        }
+      }
+    },
+
+    delta: {
+      options: {
+        livereload: true
+      },
+
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: ['jshint:gruntfile'],
+        options: {
+          livereload: false
+        }
+      },
+
+      html: {
+        files: ['<%= appFiles.html %>'],
+        tasks: ['index:development']
+      },
+
+      sass: {
+        files: ['src/sass/*.scss'],
+        tasks: ['sass']
+      },
+
+      scripts: {
+        files: ['src/app/**/*.js'],
+        tasks: ['traceur:app'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+
+    index: {
       development: {
         src: 'src/index.html',
         dest: '<%= devDir %>/',
         options: {
           beautify: true,
           relative: false,
+          logOptions: true,
           scripts: {
             vendor: ['<%= vendorFiles.js %>'],
             app: ['src/app/**/*.js']
+          },
+          styles: {
+            app: ['<%= vendorFiles.css %>', 'src/assets/<%= pkg.name %>-<%= pkg.version %>.css']
           }
         }
       },
       production: {}
     },
 
+    jshint: {
+      src: {
+        files: {
+          src: 'src/**/*.js'
+        },
+        options: {
+          jshintrc: true,
+          ignores: [
+            '**/*.spec.js',
+            '**/*.scenario.js'
+          ]
+        }
+      },
+      test: {
+        files: {
+          src: [
+            'src/**/*.spec.js',
+            'src/**/*.scenario.js'
+          ]
+        },
+        options: {
+          jshintrc: '.jshintrc.test'
+        }
+      },
+      gruntfile: [
+        'Gruntfile.js'
+      ]
+    },
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        sourceMap: true,
+        outputStyle: 'compressed'
+      },
+      dist: {
+        files: {
+          'src/assets/<%= pkg.name %>-<%= pkg.version %>.css': 'src/sass/main.scss'
+        }
+      }
+    },
+
     traceur: {
       options: {
         blockBinding: true,
-        modules: 'amd',
+        //modules: 'amd',
         moduleNames: true,
         sourceBasePackage: 'src/app'
       },
@@ -82,6 +196,17 @@ module.exports = function (grunt) {
 
   grunt.initConfig(grunt.util._.extend(taskConfig, userConfig));
 
-  // Default task(s).
-  grunt.registerTask('default', ['connect']);
+  /**
+   * The default task is to build and compile.
+   */
+  grunt.registerTask('default', ['build']);
+
+  //grunt.registerTask('e2e', ['protractor_webdriver', 'protractor']);
+
+  grunt.registerTask("bower-install", [ "bower-install-simple" ]);
+
+  grunt.renameTask('watch', 'delta');
+  grunt.registerTask('watch', ['concurrent:firstrun', 'delta']);
+
+  grunt.renameTask('htmlbuild', 'index');
 };
--- build.config.js
+++ build.config.js
@@ -2,11 +2,21 @@ module.exports = {
   devDir: 'build/development',
   prodDir: 'build/production',
 
+  appFiles: {
+    js: ['src/app/app.js', 'src/**/*.js', '!src/**/*.spec.js', '!src/**/*.scenario.js', '!src/assets/**/*.js'],
+    jsunit: ['src/**/*.spec.js', 'src/**/*.scenario.js'],
 
+    atpl: ['src/app/**/*.tpl.html'],
+    ctpl: ['src/common/**/*.tpl.html'],
 
-
+    html: ['src/index.html'],
+    sass: 'src/sass/main.sass'
+  },
 
   vendorFiles: {
+    css: [
+      'vendor/normalize-css/normalize.css'
+    ],
     js: [
       'vendor/traceur-runtime/traceur-runtime.js',
       'vendor/lodash/dist/lodash.js',
